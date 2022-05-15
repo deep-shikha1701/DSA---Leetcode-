87d728fc5155044e2e13ea65a37237dab0f1fd0d
Deepshikha Sahu <66948698+deep-shikha1701@users.noreply.github.com>
2022-05-16 00:22:49 +0530

    Create 42. Trapping Rain Water.cpp

---
+++ 42. Trapping Rain Water.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/trapping-rain-water
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left_max = 0, right_max = 0; //use a two pointer approach
+        int left = 0, right = height.size()-1;
+        int ans = 0; 
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                }else{
+                    ans +=(left_max-height[left]);
+                }
+                left++;
+            }else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                }else{
+                    ans +=(right_max-height[right]);
+                }
+                right--;
+            }
+        }
+        return ans;
+            
+    }
+};
